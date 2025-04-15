eb2f6ef895aa5506099f5489b11f26dd6d364e4d
Vadym <149205033+XXXDoriXXX@users.noreply.github.com>
2025-04-15 18:58:12 +0300

    add validation

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const Task = require('../models/task');
+const handleValidationError = require("../utils/handleValidationError");
 router.get('/', async (req, res) =>{
   const tasks = await Task.find().lean();
   res.render('index', { title: 'TODO List', tasks });
 });
 
 router.post('/add', async (req, res) =>{
-  const{title, description} = req.body;
-  await Task.create({title, description});
+  try {
+    await Task.create(req.body);
     res.redirect('/');
+  } catch (err) {
+    return handleValidationError(err, req, res, 'index');
+  }
 })
 
 router.post('/toggle/:id', async (req, res) =>{
-  const task = await Task.findById(req.params.id);
-  task.completed = !task.completed;
-  await task.save();
-  res.redirect('/');
+  try {
+    const task = await Task.findById(req.params.id);
+    task.completed = !task.completed;
+    await task.save();
+    res.redirect('/');
+  } catch (err) {
+    return handleValidationError(err, req, res, 'index');
+  }
 })
 
 router.post('/delete/:id', async (req, res) =>{
-  await Task.findByIdAndDelete(req.params.id);
-  res.redirect('/');
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+  } catch (err) {
+    return handleValidationError(err, req, res, 'index');
+  }
 })
 
 module.exports = router;
