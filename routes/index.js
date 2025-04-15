dc88f8c302ca936705f42121a0870ab16a0517fd
Vadym <149205033+XXXDoriXXX@users.noreply.github.com>
2025-04-15 18:54:11 +0300

    simple logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Task = require('../models/task');
+router.get('/', async (req, res) =>{
+  const tasks = await Task.find().lean();
+  res.render('index', { title: 'TODO List', tasks });
 });
+
+router.post('/add', async (req, res) =>{
+  const{title, description} = req.body;
+  await Task.create({title, description});
+    res.redirect('/');
+})
+
+router.post('/toggle/:id', async (req, res) =>{
+  const task = await Task.findById(req.params.id);
+  task.completed = !task.completed;
+  await task.save();
+  res.redirect('/');
+})
+
+router.post('/delete/:id', async (req, res) =>{
+  await Task.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+})
 
 module.exports = router;
